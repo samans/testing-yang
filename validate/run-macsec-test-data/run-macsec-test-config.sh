844a8c79ec4be4c63524271ebf4bfa3da6b0457b
samans <scott.mansfield@gmail.com>
2022-03-21 10:19:07 -0400

    validation tests

---
+++ validate/run-macsec-test-data/run-macsec-test-config.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/expect -f
+#set timeout -1
+spawn yanglint
+expect ">"
+send "load iana-if-type\r"
+expect ">"
+send "load ieee802-dot1q-bridge\r"
+expect ">"
+send "load ieee802-dot1q-pb\r"
+expect ">"
+send "load ieee802-dot1x\r"
+expect ">"
+send "load ieee802-dot1ae\r"
+expect ">"
+send "load ieee802-dot1ae-pry\r"
+expect ">"
+send "feature ieee802-dot1q-bridge --enable *\r"
+expect ">"
+send "feature ieee802-dot1ae-pry --enable *\r"
+expect ">"
+#send "data -t config -f json basic-vlan-bridge.xml\r"
+send "data -t config -f json -d all basic-vlan-bridge-with-macsec-config.xml\r"
+expect ">"
+send "data -t config -f json -d all basic-vlan-bridge-with-macsec-config.xml -o basic-vlan-bridge-with-macsec-config.json\r"
+expect ">"
+send "quit\r"
+expect eof
