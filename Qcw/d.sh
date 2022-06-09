e0e26917d2a2db7bcc32b5558c75a3f2f43e5a81
samans <scott.mansfield@gmail.com>
2022-06-09 17:29:22 -0400

    qcw tests

---
+++ Qcw/d.sh
@@ -0,0 +1,8 @@
+echo bridge
+diff -b ../eline/ieee802-dot1q-bridge.yang .
+echo pb
+diff -b ../eline/ieee802-dot1q-pb.yang .
+echo dotq1-types
+diff -b ../eline/ieee802-dot1q-types.yang .
+echo types
+diff -b ../eline/ieee802-types.yang .
