72344a133d7603998514221ef9f85a9f63fe5982
Scott Mansfield <scott.mansfield@gmail.com>
2024-01-16 14:48:57 -0500

    clean up of testing directory

--- Qcw/d.sh
+++
@@ -1,8 +0,0 @@
-echo bridge
-diff -b ../eline/ieee802-dot1q-bridge.yang .
-echo pb
-diff -b ../eline/ieee802-dot1q-pb.yang .
-echo dotq1-types
-diff -b ../eline/ieee802-dot1q-types.yang .
-echo types
-diff -b ../eline/ieee802-types.yang .
--- asdn/p.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -jar ~/pretty/yang802tool.jar ./ieee802-dot1as-ptp.yang -I 2 -D -W 77 -m -o ./out
