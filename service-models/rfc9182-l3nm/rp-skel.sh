ceba50bafdd873f70a6e54106bba8b7818cf5a91
Scott Mansfield <scott.mansfield@gmail.com>
2024-06-10 11:30:54 -0400

    L2 Network Model

---
+++ service-models/rfc9182-l3nm/rp-skel.sh
@@ -0,0 +1,1 @@
+pyang -f sample-xml-skeleton -p-sample-xml-skeleton-doctype=data --sample-xml-skeleton-defaults -o service-skeleton.xml -p ../includes *.yang
---
+++ service-models/rfc9182-l3nm/ylp.sh
@@ -0,0 +1,1 @@
+yanglint -p ../includes ../includes/ieee802-dot1q-types.yang ../includes/ietf-vpn-common@2022-02-11.yang ietf-l3vpn-ntw@2022-02-14.yang
